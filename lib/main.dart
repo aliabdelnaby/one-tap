e33fdc7882e02feea76d25101064471ff1a2b59f
Ali Abdelnaby <aliabdelnabi239@gmail.com>
2024-03-03 16:22:50 +0200

    add font family in main (them data)

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp.router(
       debugShowCheckedModeBanner: false,
       routerConfig: router,
+      theme: ThemeData(fontFamily: "Signika"),
     );
   }
 }
