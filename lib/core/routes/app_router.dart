aea834b45d8ffd833e394178c120d084488721ce
Ali Abdelnaby <aliabdelnabi239@gmail.com>
2024-03-03 18:15:17 +0200

    Add SlideTransition Navigate

--- lib/core/routes/app_router.dart
+++ lib/core/routes/app_router.dart
@@ -1,8 +1,8 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 import 'package:via_whatsapp/features/home/presentation/home_cubit/home_cubit.dart';
 import 'package:via_whatsapp/features/home/presentation/views/home_view.dart';
-import 'package:via_whatsapp/features/settings/presentation/settings_cubit/settings_cubit.dart';
 import 'package:via_whatsapp/features/settings/presentation/views/select_language_view.dart';
 import 'package:via_whatsapp/features/settings/presentation/views/settings_view.dart';
 
@@ -17,14 +17,39 @@ final GoRouter router = GoRouter(
     ),
     GoRoute(
       path: '/settingsView',
-      builder: ((context, state) => BlocProvider(
-            create: (context) => SettingsCubit(),
-            child: const SettingsView(),
-          )),
+      pageBuilder: (context, state) {
+        return CustomTransitionPage(
+          key: state.pageKey,
+          child: const SettingsView(),
+          transitionsBuilder: (context, animation, secondaryAnimation, child) {
+            return SlideTransition(
+              position: Tween<Offset>(
+                begin: const Offset(2.0, 0.0), // Start position (right)
+                end: Offset.zero, // End position (left)
+              ).animate(animation),
+              child: child,
+            );
+          },
+        );
+      },
     ),
     GoRoute(
       path: '/selectLanguageView',
-      builder: ((context, state) => const SelectLanguageView()),
+      pageBuilder: (context, state) {
+        return CustomTransitionPage(
+          key: state.pageKey,
+          child: const SelectLanguageView(),
+          transitionsBuilder: (context, animation, secondaryAnimation, child) {
+            return SlideTransition(
+              position: Tween<Offset>(
+                begin: const Offset(2.0, 0.0), // Start position (right)
+                end: Offset.zero, // End position (left)
+              ).animate(animation),
+              child: child,
+            );
+          },
+        );
+      },
     ),
   ],
 );
