6f32f7c11ec5ae615f5c1013e3607c8964ee6618
Ali Abdelnaby <aliabdelnabi239@gmail.com>
2024-03-04 20:00:47 +0200

    Register contact type adapter

--- lib/features/home/cubits/add_contact_cubit/add_contact_cubit.dart
+++ lib/features/home/cubits/add_contact_cubit/add_contact_cubit.dart
@@ -1,7 +1,12 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:one_tap/core/models/contact_model.dart';
 import 'package:one_tap/features/home/cubits/add_contact_cubit/add_contact_state.dart';
 
 class AddContactCubit extends Cubit<AddContactState> {
   AddContactCubit() : super(AddContactInitial());
 
+addContact(ContactModel contact)
+{
+  
+}
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,16 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:one_tap/core/constants/constants.dart';
+import 'package:one_tap/core/models/contact_model.dart';
 import 'core/app/app.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await EasyLocalization.ensureInitialized();
   await Hive.initFlutter();
-  await Hive.openBox('contacts');
+  await Hive.openBox(kContacts);
+  Hive.registerAdapter(ContactModelAdapter());
   runApp(
     EasyLocalization(
       supportedLocales: const [Locale('en'), Locale('ar')],
