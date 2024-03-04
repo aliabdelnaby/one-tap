6605f016e8411748282d75fe24684e3adf04afb9
Ali Abdelnaby <aliabdelnabi239@gmail.com>
2024-03-04 17:21:00 +0200

    Add Hive package

--- lib/core/models/contact_model.dart
+++ lib/core/models/contact_model.dart
@@ -2,5 +2,8 @@ class ContactModel {
   final String number;
   final String date;
 
-  ContactModel({required this.number, required this.date});
+  ContactModel({
+    required this.number,
+    required this.date,
+  });
 }
