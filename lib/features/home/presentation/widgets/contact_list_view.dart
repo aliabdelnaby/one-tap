7fc399bbba672a72d95def33765ad6eb66c0b52a
Ali Abdelnaby <aliabdelnabi239@gmail.com>
2024-03-04 16:37:10 +0200

    Contacts List View

---
+++ lib/features/home/presentation/widgets/contact_list_view.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:one_tap/features/home/presentation/widgets/custom_contact_item.dart';
+
+class ContactsListView extends StatelessWidget {
+  const ContactsListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: 10,
+      shrinkWrap: true,
+      itemBuilder: ((context, index) {
+        return const CustomContactItem();
+      }),
+    );
+  }
+}
--- lib/features/home/presentation/widgets/home_view_body.dart
+++ lib/features/home/presentation/widgets/home_view_body.dart
@@ -2,7 +2,7 @@ import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:one_tap/core/utils/app_text_styles.dart';
-import 'package:one_tap/features/home/presentation/widgets/custom_contact_item.dart';
+import 'package:one_tap/features/home/presentation/widgets/contact_list_view.dart';
 import 'package:one_tap/features/home/presentation/widgets/custom_enter_mobile_nember_and_open_tele_whatsapp.dart';
 import '../../home_cubit/home_cubit.dart';
 import '../../home_cubit/home_state.dart';
@@ -34,7 +34,7 @@ class HomeViewBody extends StatelessWidget {
                   "Recent Conversation".tr(),
                   style: CustomTextStyle.signikastyle18,
                 ),
-                const CustomContactItem(),
+                const Expanded(child: ContactsListView()),
               ],
             ),
           ),
