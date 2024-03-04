1b3c8a1d80f79e5c933b4608ba04cd8ec0e209de
Ali Abdelnaby <aliabdelnabi239@gmail.com>
2024-03-04 20:53:17 +0200

    add BlocConsumer to EnterMobileNumberSection

--- lib/features/home/presentation/widgets/home_view_body.dart
+++ lib/features/home/presentation/widgets/home_view_body.dart
@@ -1,6 +1,11 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:one_tap/core/functions/custom_toast.dart';
+import 'package:one_tap/core/utils/app_colors.dart';
 import 'package:one_tap/core/utils/app_text_styles.dart';
+import 'package:one_tap/features/home/cubits/home_cubit/home_cubit.dart';
+import 'package:one_tap/features/home/cubits/home_cubit/home_state.dart';
 import 'package:one_tap/features/home/presentation/widgets/contact_list_view.dart';
 import 'package:one_tap/features/home/presentation/widgets/custom_enter_mobile_nember_and_open_tele_whatsapp.dart';
 
@@ -14,7 +19,19 @@ class HomeViewBody extends StatelessWidget {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          const EnterMobileNumberSection(),
+          BlocConsumer<HomeCubit, HomeState>(
+            listener: (context, state) {
+              if (state is AddContactFailure) {
+                showToast(state.errMessage, AppColors.primaryColor);
+                print("*****Failure ${state.errMessage}*****");
+              } else if (state is AddContactSuccess) {
+                print("*****Success Add Contact*****");
+              }
+            },
+            builder: (context, state) {
+              return const EnterMobileNumberSection();
+            },
+          ),
           const SizedBox(height: 20),
           const Divider(),
           Text(
